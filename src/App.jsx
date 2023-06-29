218a06351c152204148c3cf047708efca38350f2
BrendaMartinez28 <125223171+BrendaMartinez28@users.noreply.github.com>
2023-06-29 19:39:00 -0300

    Segunda preentrega

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import './App.css';
 import './components/CartWidget/CartWidget.css'
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Contador from './components/Contador/Contador';
+import Categorias from './components/Categorias/Categorias';
 
 function App() {
   return (
     <div className="App">
+   
     <h1 style={{color:"blueviolet"}}>BIENVENIDOS A PERITASTECH</h1>
-    <NavBar/>
+  
+    <NavBar/> 
+    <Categorias/> 
+    <Contador stock={10} inicial={1}/>
     <ItemListContainer greeting={'LO ULTIMO EN TECNOLOGIA'}/>
+   
     </div>
-    
+     
   );
 }
 
---
+++ src/components/Categorias/Categorias.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react"
+
+const Categorias = () => {
+    const[categoria, setCategoria] = useState("");
+
+    useEffect( ()=> {
+        document.title = `Categoria ${categoria}`;
+    }, [categoria])
+
+    const handleClick = (categoria) => {
+        setCategoria(categoria);
+    }
+
+  return (
+    <div>
+        <h2>Catergoria de productos</h2>
+        <button onClick={()=> handleClick("Perifericos")}>Perifericos</button>
+        <button onClick={()=> handleClick("Placas de video")}>Placas de video</button>
+        <button onClick={()=> handleClick("Sillas gamer")}>Sillas gamer </button>
+    </div>
+  )
+}
+
+export default Categorias
